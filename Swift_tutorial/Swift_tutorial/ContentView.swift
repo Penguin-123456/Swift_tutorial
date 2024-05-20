b38ea4bfcf28bb91c964789b068fd62a0648eed1
Penguin-123456 <151092188+Penguin-123456@users.noreply.github.com>
2024-05-20 18:13:30 +0900

    Swift Initialize

---
+++ Swift_tutorial/Swift_tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift_tutorial
+//
+//  Created by 岡崎義弘 on 2024/05/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift_tutorial/Swift_tutorial/Swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_tutorialApp.swift
+//  Swift_tutorial
+//
+//  Created by 岡崎義弘 on 2024/05/20.
+//
+
+import SwiftUI
+
+@main
+struct Swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
