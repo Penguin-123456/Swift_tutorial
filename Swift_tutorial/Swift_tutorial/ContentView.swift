5fc29be841ba827d51071d75ed3f667d73e43f33
Penguin-123456 <151092188+Penguin-123456@users.noreply.github.com>
2024-05-21 17:52:18 +0900

    Hello,SwiftUI

--- Swift_tutorial/Swift_tutorial/ContentView.swift
+++ Swift_tutorial/Swift_tutorial/ContentView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var str = "Hello, SwiftUI"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("こんにちは世界")
+            Text(str)
             Button("ボタン") {
+                str = "ハローSwiftUI"
                 print("ボタンが押されたよ")
             }
         }
