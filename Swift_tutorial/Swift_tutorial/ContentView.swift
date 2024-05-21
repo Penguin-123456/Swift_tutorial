4c34d5169b1c650098b55d3a4373216ae2075bee
Penguin-123456 <151092188+Penguin-123456@users.noreply.github.com>
2024-05-21 17:50:13 +0900

    button

--- Swift_tutorial/Swift_tutorial/ContentView.swift
+++ Swift_tutorial/Swift_tutorial/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("こんにちは世界")
+            Button("ボタン") {
+                print("ボタンが押されたよ")
+            }
         }
         .padding()
     }
